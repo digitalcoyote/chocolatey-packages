da1318bfb47bf7c6d55ec7f1b6bd6a6afee35b3f
Chocolatey <chocolatey@realdimensions.net>
2021-03-26 21:33:37 +0000

    AU: 2 updated - dip oh-my-posh [skip ci]

--- dip/tools/chocolateyinstall.ps1
+++ dip/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'dip.exe'
-    URL = 'https://github.com/030/dip/releases/download/2.1.2/dip-windows'
-    Checksum = 'c9e118f7699fd11fea1864b87a0502981223f6bea7a0a0d13e2a90843abfa7b6778cafedcc496829aeaed4aea8e267233b80001f3c850592980e05956171f33b'
+    URL = 'https://github.com/030/dip/releases/download/2.1.4/dip-windows-2019'
+    Checksum = 'afc0395435776d7fffc55d08b61610303d3f1d4667abf52463f9b00f74a34b97e645c76ad195037e7819c0d6eea85ade3e1a7da0fc173839913c4e28d7c6c885'
     ChecksumType = 'sha512'
 }
 
--- oh-my-posh/tools/chocolateyinstall.ps1
+++ oh-my-posh/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $InstallArgs = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'exe'
   SilentArgs = '/VERYSILENT'
-  Url64bit = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v3.122.1/install.exe'
-  Checksum64 = 'b092cf5af905d83f1e0ffcf6bd6128e833c9dfb0d4b6b9ae22f70870cd27280a6eaa487a8db6808832d3e0a6b04a865d3039707c73701e59df0310ea6e29b962'
+  Url64bit = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v3.123.0/install.exe'
+  Checksum64 = 'aea2bf11cb4a8312d681de8181d147f52fd9a8a3a4c8a815109dda78092bd3069e234351b773c11660b164e3fb83b8849151de341fa04edd20e540b314b15c12'
   ChecksumType64 = 'sha512'
 }
 
