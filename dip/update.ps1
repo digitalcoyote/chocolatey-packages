8da6adf90b67a6cf92230ecce5d1d0db363c7da1
Ben van B <030@users.noreply.github.com>
2021-03-26 21:57:47 +0100

    [GH-19] Resolve broken update due to binary naming change

--- dip/update.ps1
+++ dip/update.ps1
@@ -14,10 +14,10 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $regex   = '/030/dip/tree/[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}.*'
+  $regex   = '/030/dip/releases/download/[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}.*/dip-windows.*'
   $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
-  $version = $url -split '\/' | Select-Object -Last 1
-  $url = "https://github.com/030/dip/releases/download/$version/dip-windows"
+  $version = $url -split '\/' | Select-Object -Last 1 -Skip 1
+  $url = "https://github.com$url"
   return @{ Version = $version; URL = $url; ChecksumType32 = 'sha512';}
 }
 
