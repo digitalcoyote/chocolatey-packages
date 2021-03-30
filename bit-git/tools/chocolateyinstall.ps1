b436b66bab2bc0d29ecc330f9d3b05ea838b9e6f
Chocolatey <chocolatey@realdimensions.net>
2021-03-30 16:06:55 +0000

    AU: 2 updated - bit-git nethermind [skip ci]

--- bit-git/tools/chocolateyinstall.ps1
+++ bit-git/tools/chocolateyinstall.ps1
@@ -6,10 +6,10 @@ $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:Chocolat
 $ChocoWebFileArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileFullPath = (Join-Path $UnzipLocation 'bit-git.tar.gz')
-    Url = 'https://github.com/chriswalz/bit/releases/download/v1.0.5/bit_1.0.5_windows_386.tar.gz'
-    Url64bit = 'https://github.com/chriswalz/bit/releases/download/v1.0.5/bit_1.0.5_windows_amd64.tar.gz'
-    CheckSum = '949724d2950ba3f6a4be8b6d8d0f0090e0c9537e40ecedf78ed9f8cf83a5ee4f91d549e69e08541461197aa35283b3473aa64a7f20d111923c38872758f3b7be'
-    Checksum64 = 'fdb6f56d6f7bf1067355e5eb93f5b3a8cbac346310f63b3fc46498862f620b5758825b679be4b481290a183e6e60b7fbd1d01996baad63c4650b29e8a5b61ac8'
+    Url = 'https://github.com/chriswalz/bit/releases/download/v1.0.6/bit_1.0.6_windows_386.tar.gz'
+    Url64bit = 'https://github.com/chriswalz/bit/releases/download/v1.0.6/bit_1.0.6_windows_amd64.tar.gz'
+    CheckSum = '2db67aa02558dd13790a54239e207fabb543be80459953a5f54af71268d4b86cdc9a2cdd15761536290f70ca960392d195257e71c417168eec53330f3e819ad0'
+    Checksum64 = '7279b113f352a5e4275f1fdf67f86bbb65582ba6fc72f5ff3818cbb14147d7f14cff78a76434f1859abd6c3d1e250cd2f98d0106d36775fe66828a9c0665315c'
     CheckSumType = 'sha512'
     ChecksumType64 = 'sha512'
 }
--- nethermind/tools/chocolateyinstall.ps1
+++ nethermind/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $ZipInstallArgs = @{
     PackageName = $env:ChocolateyPackageName 
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = 'https://github.com/NethermindEth/nethermind/releases/download/1.10.50/nethermind-windows-amd64-1.10.50-7b1a10f-20210329.zip'
-    CheckSum64 = '743313e48d9466cbfeadf9982d447094ac4d0c8ee7946749659b2d96b4482669f3f418ce78481dc0453ceae6be19145c544722e6f97382afe043b61cdb2c1015'
+    Url64 = 'https://github.com/NethermindEth/nethermind/releases/download/1.10.51/nethermind-windows-amd64-1.10.51-1a20c48-20210330.zip'
+    CheckSum64 = '6a4144a278b4018acf75cce124fa17034d0080247621c37c5352fadda3abf3629b1ffd19dba8e69b7d1ae70165cfd06d87c3461821c7e93d610abcdb6a3998a0'
     CheckSumType64 = 'sha512'
 }
 
