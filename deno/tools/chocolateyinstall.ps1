47f369164f91f1cda159e0bcabec317cd78a9a10
Chocolatey <chocolatey@realdimensions.net>
2021-05-18 00:06:48 +0000

    AU: 2 updated - deno nethermind [skip ci]

--- deno/tools/chocolateyinstall.ps1
+++ deno/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64 = 'https://github.com/denoland/deno/releases/download/v1.10.1/deno-x86_64-pc-windows-msvc.zip'
-$checksum64 = '0445a8107264fa1aa909b46a5b7e255ad2647feecd34e5684f87621786c50f7f7afbc2b84f9b149369f531b72024d198ccd92b2823ffc53acd5ff4334f36b9db'
+$url64 = 'https://github.com/denoland/deno/releases/download/v1.10.2/deno-x86_64-pc-windows-msvc.zip'
+$checksum64 = 'e4bcba5efa77657d7fc7f3f8962838ea5101e6cf34b8971211d7a02eede8769e672167fbb8df8b1831789ea935c7a3cecf5d7688c9b7756c1918368222441fca'
 $checksumType64 = 'sha512'
 $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
 Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -Url64 $url64 -UnzipLocation $UnzipLocation -CheckSum64 $checksum64 -CheckSumType64 $checksumType64
--- nethermind/tools/chocolateyinstall.ps1
+++ nethermind/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $ZipInstallArgs = @{
     PackageName = $env:ChocolateyPackageName 
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = 'https://github.com/NethermindEth/nethermind/releases/download/1.10.70/nethermind-windows-amd64-1.10.70-9837865-20210513.zip'
-    CheckSum64 = 'b0c0d12e9e4b8abc0a7f8d8b8c000e5467659917cec13d09538ac460ff3d2796fd3a675a5330bde9ade431ba6c589333d4bd844b43f374bd742d81f12b135ac0'
+    Url64 = 'https://github.com/NethermindEth/nethermind/releases/download/1.10.71/nethermind-windows-amd64-1.10.71-13221de-20210517.zip'
+    CheckSum64 = 'ad8c9f0256008cb0d11eef1e84b1e6f034a018445fc520bcbe20c51e88aff63a838f5051f0c3df030985b9f380b5634053f18c2afb69afaf82ea492abb9671ac'
     CheckSumType64 = 'sha512'
 }
 
