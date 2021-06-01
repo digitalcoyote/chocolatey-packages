f14aee5eb8782f812755ddb58e04a6c64bc5a0a5
Chocolatey <chocolatey@realdimensions.net>
2021-06-01 00:06:36 +0000

    AU: 2 updated - deno dip [skip ci]

--- deno/tools/chocolateyinstall.ps1
+++ deno/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64 = 'https://github.com/denoland/deno/releases/download/v1.10.2/deno-x86_64-pc-windows-msvc.zip'
-$checksum64 = 'e4bcba5efa77657d7fc7f3f8962838ea5101e6cf34b8971211d7a02eede8769e672167fbb8df8b1831789ea935c7a3cecf5d7688c9b7756c1918368222441fca'
+$url64 = 'https://github.com/denoland/deno/releases/download/v1.10.3/deno-x86_64-pc-windows-msvc.zip'
+$checksum64 = '9a8c8103947816d931fbd0a7e5965fd3a937068a3660637d044992de2e4b1b9d4d3a997ab01340ce0146feca7d9cc78f03ab2da838b27e0e3833b02a6f0a0e54'
 $checksumType64 = 'sha512'
 $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
 Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -Url64 $url64 -UnzipLocation $UnzipLocation -CheckSum64 $checksum64 -CheckSumType64 $checksumType64
--- dip/tools/chocolateyinstall.ps1
+++ dip/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'dip.exe'
-    URL = 'https://github.com/030/dip/releases/download/2.1.4/dip-windows-2019'
-    Checksum = 'afc0395435776d7fffc55d08b61610303d3f1d4667abf52463f9b00f74a34b97e645c76ad195037e7819c0d6eea85ade3e1a7da0fc173839913c4e28d7c6c885'
+    URL = 'https://github.com/030/dip/releases/download/2.1.5/dip-windows-2019'
+    Checksum = '9803089de6c3319946cb46a28eefdbe6d2b92ce6c974da2035b042e92590d82c06cf58dda8441221fc0506076d5b222960284a87527b9e62c448ea33bb0afb89'
     ChecksumType = 'sha512'
 }
 
