2b2c493415b55c33c29511ed971b0811a7b3ccb0
Chocolatey <chocolatey@realdimensions.net>
2021-07-25 15:05:29 +0000

    AU: 1 updated - docto [skip ci]

--- docto/tools/chocolateyinstall.ps1
+++ docto/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
-    URL = 'https://github.com/tobya/DocTo/releases/download/V1.3/docto.zip'
+    URL = 'https://github.com/tobya/DocTo/releases/download/V1.5/docto105.zip'
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Checksum = '1bc5ad1da8a405d8b447ebf7a0cff01c63faa7b882ec0d605214770a12a85d25f0c1cc280e9a5bf6e593665cc2c786fede7714c879683f10c63ef32af8132bfd'
+    Checksum = '872558d1b220824d4ca2155de56694211b1b62441a8a7286fc5df0f0b0e6abc08ea505ecf8a3e7fe15eacb4fbfdb62a036508f4d7c531e459b17524ad2cb1dca'
     ChecksumType = 'sha512'
 }
 
