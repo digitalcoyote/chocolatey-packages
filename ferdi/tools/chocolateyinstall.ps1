3a8950d958aa414b9c9fdd93ee551606a8503d0a
Vijay A <avijayr@protonmail.com>
2021-07-26 18:10:31 +0530

    Upgraded ferdi to 5.6.0

--- ferdi/tools/chocolateyinstall.ps1
+++ ferdi/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$args = @{ 
+$args = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'exe'
   SilentArgs = '/S'
-  Url64bit = 'https://github.com/getferdi/ferdi/releases/download/v5.5.0/Ferdi-Setup-5.5.0.exe'
-  Checksum64 = '3c7ca5709122056f19d824d9e3f7ee3ac890f423d7d260cf4e343f7e397473c661abfdb788fb0661c49013cf575d9b77ea672aad6841f47c53ab0472084d2fe9'
+  Url64bit = 'https://github.com/getferdi/ferdi/releases/download/v5.6.0/Ferdi-Setup-5.6.0.exe'
+  Checksum64 = 'bcceb50a42e0ea3aefe2b10fdfe6eb336eb1d0bb6868386017928d52e73dd3ebd2ee7aa36c215f1cd40b0c219adb5a5d5821d483af54d0e36e44e9b9f7757add'
   ChecksumType64 = 'sha512'
 }
 
