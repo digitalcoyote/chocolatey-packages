5017245fdc497614edcf6bbb5bada3f421465c94
Chocolatey <chocolatey@realdimensions.net>
2021-08-09 00:06:49 +0000

    AU: 1 updated - authpass [skip ci]

--- authpass/tools/chocolateyinstall.ps1
+++ authpass/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $args = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'exe'
   SilentArgs = '/VERYSILENT'
-  Url64bit = 'https://github.com/authpass/authpass/releases/download/v1.9.0/AuthPass-setup-1.9.0_1762.exe'
-  Checksum64 = 'ad9ab023de100ff9a22eb1f13c42aea9e7c55afebc307dd76aa6f8a3feb7a44822c8fc133bba038047a0e8ed779e1ffdf1ef0f6c0b70e60a73dbb7d07fed8ecd'
+  Url64bit = 'https://github.com/authpass/authpass/releases/download/v1.9.1/AuthPass-setup-1.9.1_1813.exe'
+  Checksum64 = 'e29d338d50ade5cfe02e911d11fcb735725cf65290ffccde48e109527d63e9e815a5ef2771b9db5cd3b10e845b8964c99c668f688a04b7d8cb1e321c7ed2cb4e'
   ChecksumType64 = 'sha512'
 }
 
