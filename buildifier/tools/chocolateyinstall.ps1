2ad3f49b57d8f3701d51771abebf20cfe7c6c608
Curtis Carter <lokiphasma@gmail.com>
2021-08-19 18:15:59 -0700

    Added buildifier

---
+++ buildifier/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$InstallArgs = @{
+    PackageName = $env:ChocolateyPackageName
+    FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'bazelisk.exe'
+    URL64 = 'https://github.com/bazelbuild/buildtools/releases/download/4.0.1/buildifier-windows-amd64.exe'
+    Checksum64 = ''
+    ChecksumType64 = 'sha512'
+}
+
+Get-ChocolateyWebFile @InstallArgs
