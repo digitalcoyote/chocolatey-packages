d555eab1fae9a2aa5845045e228bc56166887e8f
Curtis Carter <lokiphasma@gmail.com>
2021-08-19 18:40:41 -0700

    Added unused_deps

---
+++ unused_deps/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$InstallArgs = @{
+    PackageName = $env:ChocolateyPackageName
+    URL64 = 'https://github.com/bazelbuild/buildtools/releases/download/4.0.1/buildifier-windows-amd64.exe'
+    FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'unused_deps.exe'
+    Checksum64 = ''
+    ChecksumType64 = 'sha512'
+}
+
+Get-ChocolateyWebFile @InstallArgs
---
+++ unused_deps/update.ps1
@@ -0,0 +1,25 @@
+import-module au
+
+$releases = "https://github.com/bazelbuild/buildtools/releases/"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(URL64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+      "(Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(ChecksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '/bazelbuild/buildtools/releases/tag/[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}.*'
+  $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/' | Select-Object -Last 1
+  $url = "https://github.com/bazelbuild/buildtools/releases/download/$version/unused_deps-windows-amd64.exe
+"
+  return @{ Version = $version; URL64 = $url; ChecksumType64 = 'sha512';}
+}
+
+Update-Package -ChecksumFor 64
