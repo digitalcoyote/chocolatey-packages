fb116eeae87e6a4ca4e53c5a8eabd41e2f7cf928
Curtis Carter <lokiphasma@gmail.com>
2021-08-19 18:32:53 -0700

    Correcting executable names
    buildifier and buildozer accidentally left as bazelisk

--- buildozer/tools/chocolateyinstall.ps1
+++ buildozer/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
-    FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'bazelisk.exe'
     URL64 = 'https://github.com/bazelbuild/buildtools/releases/download/4.0.1/buildifier-windows-amd64.exe'
+    FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'buildozer.exe'
     Checksum64 = ''
     ChecksumType64 = 'sha512'
 }
