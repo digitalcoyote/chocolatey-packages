71d48870795002c548362249a65cb24e2e27d6a9
Chocolatey <chocolatey@realdimensions.net>
2021-09-06 16:06:36 +0000

    AU: 2 updated - buildifier buildozer [skip ci]

--- buildifier/tools/chocolateyinstall.ps1
+++ buildifier/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'buildifier.exe'
-    URL64 = 'https://github.com/bazelbuild/buildtools/releases/download/4.0.1/buildifier-windows-amd64.exe'
-    Checksum64 = '7973d1dc43795e0611dc3e525be7e6656e51622abc3d982de07f3a582b2090db10f8233589294a35ccd7108ed305e1f69f750670d9125917c45d483c378ac784'
+    URL64 = 'https://github.com/bazelbuild/buildtools/releases/download/4.2.0/buildifier-windows-amd64.exe'
+    Checksum64 = '1fefc1e52a547244b1d9093eeee672887a5c7d2920ab81a84757357f08eac21ca8c248be4ca81362755cc6f0f386cc47f6ff18395fd75a54cf087b4db914be1d'
     ChecksumType64 = 'sha512'
 }
 
--- buildozer/tools/chocolateyinstall.ps1
+++ buildozer/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
-    URL64 = 'https://github.com/bazelbuild/buildtools/releases/download/4.0.1/buildozer-windows-amd64.exe'
+    URL64 = 'https://github.com/bazelbuild/buildtools/releases/download/4.2.0/buildozer-windows-amd64.exe'
     FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'buildozer.exe'
-    Checksum64 = '8a508504143b466c107a87d278cbd0f881e24bbe11e3631fb9cdb6454973114dc1aa35132129fb0aef3c1d2b6d07b6c23cae15bde8539cccfaed91bea9c94a40'
+    Checksum64 = 'e6adb0e1cd49507f57973483f7ffdcd286984c22f7b82acb72b071949ecf28e5460fe6d30e93d4eb207c6680085b303357696e3a18cbe33a373fbe5be129ab31'
     ChecksumType64 = 'sha512'
 }
 
