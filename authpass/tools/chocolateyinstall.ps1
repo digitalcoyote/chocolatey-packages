0e902a99763512faee50f3ff2c5d0a14175e77e2
Chocolatey <chocolatey@realdimensions.net>
2021-09-12 22:18:56 +0000

    AU: 2 updated - authpass ferdi [skip ci]

--- authpass/tools/chocolateyinstall.ps1
+++ authpass/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $args = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'exe'
   SilentArgs = '/VERYSILENT'
-  Url64bit = 'https://github.com/authpass/authpass/releases/download/v1.9.3/AuthPass-setup-1.9.3_1848.exe'
-  Checksum64 = '612c94c387b5cdeb9e5f0aa01cf28391054285f2b6cc40854a22deff9988d1bf9d838c8dbca38cb75d484e7b8124f5eea358cab19e2a63832992dfb7635a587c'
+  Url64bit = 'https://github.com/authpass/authpass/releases/download/v1.9.4/AuthPass-setup-1.9.4_1872.exe'
+  Checksum64 = '7ec17b4bc3d99b8743a3af2d331c5fd71e6efabac5e1a033bef3b8c668c251a1dd78e646d77ec0efb29ea3ed97ad9bac4bb7e58d8db0e4a3b5c71eb4f9e4b252'
   ChecksumType64 = 'sha512'
 }
 
--- ferdi/tools/chocolateyinstall.ps1
+++ ferdi/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $args = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'exe'
   SilentArgs = '/S'
-  Url64bit = 'https://github.com/getferdi/ferdi/releases/download/v5.6.0/Ferdi-Setup-5.6.0.exe'
-  Checksum64 = 'bcceb50a42e0ea3aefe2b10fdfe6eb336eb1d0bb6868386017928d52e73dd3ebd2ee7aa36c215f1cd40b0c219adb5a5d5821d483af54d0e36e44e9b9f7757add'
+  Url64bit = 'https://github.com/getferdi/ferdi/releases/download/v5.6.1/Ferdi-5.6.1.exe'
+  Checksum64 = '3781ae56da5cd69f6255d18fba97dcd4343bdcca26e0d503bb3d6696ac9fc113fb2d96feca506514a06249db19a5ec42de72b3ea554841ecedf77cfff339eda8'
   ChecksumType64 = 'sha512'
 }
 
