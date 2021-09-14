481df717d53552cf164f00450f365ab55a2f9371
Curtis Carter <lokiphasma@gmail.com>
2021-09-14 14:50:41 -0700

    Updated Ferdi to use msi installer
    Updated ferdi.nuspec with info from current ReadMe
    Switched to .msi because nsis installer was no longer silent

--- ferdi/tools/chocolateyinstall.ps1
+++ ferdi/tools/chocolateyinstall.ps1
@@ -2,11 +2,14 @@
 
 $args = @{ 
   PackageName = $env:ChocolateyPackageName
-  FileType = 'exe'
-  SilentArgs = '/S'
-  Url64bit = 'https://github.com/getferdi/ferdi/releases/download/v5.6.2/Ferdi-5.6.2.exe'
-  Checksum64 = 'db51ce229955bc2c898d0cf62065353502f6f2bd03eb00de4c46b24bc1f0aca4286c9a5d65b3243ec704b9be726cc31dbc062c0a929ca0281b47048e71311480'
+  FileType = 'msi'
+  SilentArgs = '/qn'
+  Url64bit = 'https://github.com/getferdi/ferdi/releases/download/v5.6.2/Ferdi-5.6.2.msi'
+  Url = 'https://github.com/getferdi/ferdi/releases/download/v5.6.2/Ferdi-5.6.2-ia32.msi'
+  Checksum64 = '0803279da481056229a95041bcab816adc9b779531d6e6e3512ea2e41d50b74bb193803421277135f1b03756d0513858a4e8d01f545332675f5174866b24559d'
+  Checksum = '1343ffa8c23b801caf4c923f89c2b9d1ff8b620384d1edf610783fcf1407d84d49d3a897caac6e9b597e6138c69b293e89b9242bbe9d3ae3998b4efe128353f7'
   ChecksumType64 = 'sha512'
+  ChecksumType = 'sha512'
 }
 
 Install-ChocolateyPackage  @args
--- ferdi/update.ps1
+++ ferdi/update.ps1
@@ -6,21 +6,25 @@ function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
       "(Url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(Url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
       "(Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
       "(ChecksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+      "(ChecksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
     }
   }
 }
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $regex   = '\/getferdi\/ferdi\/releases\/download\/v\d{1,4}\.\d{1,4}\.\d{1,4}.*\/Ferdi-.*\.exe$'
-  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
-  $url = "https://github.com$url"
-  $version = $url -split '\/|v' | Select-Object -Last 1 -Skip 1
+  $regex   = '\/getferdi\/ferdi\/releases\/download\/v\d{1,4}\.\d{1,4}\.\d{1,4}.*\/Ferdi-\d{1,4}\.\d{1,4}\.\d{1,4}\.msi$'
+  $url64     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $url64 = "https://github.com$url64"
+  $version = $url64 -split '\/|v' | Select-Object -Last 1 -Skip 1
+  $url32 = "https://github.com/getferdi/ferdi/releases/download/v$version/Ferdi-$version-ia32.msi"
   $releaseNotes = "https://github.com/getferdi/ferdi/releases/tag/v$version"
 
-  return @{ Version = $version; URL64 = $url; ChecksumType64 = 'sha512'; ReleaseNotes = $releaseNotes}
+  return @{ Version = $version; URL64 = $url64; URL = $url32; ChecksumType64 = 'sha512'; ChecksumType32 = 'sha512'; ReleaseNotes = $releaseNotes}
 }
 
-Update-Package -ChecksumFor 64
+Update-Package -ChecksumFor all
