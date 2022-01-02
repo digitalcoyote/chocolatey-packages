4aebf0eb7bae339f720fb9b4291c7e10cde051d7
Chocolatey <chocolatey@realdimensions.net>
2022-01-02 16:06:49 +0000

    AU: 1 updated - ferdi [skip ci]

--- ferdi/tools/chocolateyinstall.ps1
+++ ferdi/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $args = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'msi'
   SilentArgs = '/qn'
-  Url64bit = 'https://github.com/getferdi/ferdi/releases/download/v5.6.6/Ferdi-5.6.6.msi'
-  Url = 'https://github.com/getferdi/ferdi/releases/download/v5.6.6/Ferdi-5.6.6-ia32.msi'
-  Checksum64 = 'f4806d07c2fd946ded8548511844d632bfa75b8772a30e0a0ead6ea9c1f5d362b18626c56436aa9e5a1750126b82737ac6408dbfeaaf4f2bbbb25c44daf651e5'
-  Checksum = 'c43b27bf73a44943a77c71f157b7604be038581c5e922b8d51f410a1b9ed34df62a553ffedc393daf87ee13f748a7c322b696286fef5a205a18d29987149e87c'
+  Url64bit = 'https://github.com/getferdi/ferdi/releases/download/v5.6.7/Ferdi-5.6.7.msi'
+  Url = 'https://github.com/getferdi/ferdi/releases/download/v5.6.7/Ferdi-5.6.7-ia32.msi'
+  Checksum64 = 'e7c63bdec942d160e60d50c17d78ae9568380698b426182c73fde1e3a8fd2f25b3af9f878c2dd3baceb1d69d4451a824e579d21899072c25b68f9fde639c4e87'
+  Checksum = 'e78f3ee8c286d72c577083fa94db96c62d356e358d11316b6f7a36c39a3310b28945489dfcfd173f3c00b2c42c5d24b6ec08c2fe6288508b22124b75f9971374'
   ChecksumType64 = 'sha512'
   ChecksumType = 'sha512'
 }
