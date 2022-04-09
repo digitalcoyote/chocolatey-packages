f7dead41bd198f4711e51ce0400d900516df6e56
codingcoyote <coyote@duck.com>
2022-04-08 17:10:26 -0700

    Added Winpinator

---
+++ winpinator/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url           = 'https://github.com/swiszczoo/winpinator/releases/download/v0.1.2/winpinator_setup_0.1.2_x86.exe'
+  url64          = 'https://github.com/swiszczoo/winpinator/releases/download/v0.1.2/winpinator_setup_0.1.2_x64.exe'
+  checksum32      = ''
+  checksum64      = 'db19319a39bc8b038455c87a578c04624ff1103cac489f33b2305637557c54df6a50bf9e91288df47a3f4c90208d44a0c6bb23a46cb67475ba34789a08c8b197'
+  checksumType  = 'sha512'
+  checksumType64  = 'sha512'
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+#Create .ignore files so chocolatey does not shim the Exe
+$files = get-childitem $toolsDir -include *.exe -recurse
+foreach ($file in $files) {
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
---
+++ winpinator/update.ps1
@@ -0,0 +1,31 @@
+﻿import-module au
+
+$releases = "https://github.com/swiszczoo/winpinator/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(Url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(Url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(CheckSum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(CheckSum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(CheckSumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType)'"
+      "(CheckSumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/swiszczoo\/winpinator\/releases\/download\/v\d{1,3}\.\d{1,3}\.\d{1,3}.*\/winpinator_setup_\d{1,3}\.\d{1,3}\.\d{1,3}.*_x86\.exe$'
+  $regex64 = '\/swiszczoo\/winpinator\/releases\/download\/v\d{1,3}\.\d{1,3}\.\d{1,3}.*\/winpinator_setup_\d{1,3}\.\d{1,3}\.\d{1,3}.*_x64\.exe$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $url64   = $download_page.links | Where-Object href -match $regex64 | Select-Object -First 1 -expand href
+  $version = $url -split '\/|v' | Select-Object -Last 1 -Skip 1
+  $url = "https://github.com$url"
+  $url64 = "https://github.com$url64"
+  return @{ Version = $version; URL64 = $url64; URL = $url; ChecksumType= 'sha512'; ChecksumType64 = 'sha512';}
+}
+
+Update-Package -ChecksumFor 64
