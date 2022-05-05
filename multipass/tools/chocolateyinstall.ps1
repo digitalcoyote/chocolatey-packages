8ab0ff4a59164038025688eb5f5e4972327b1fb8
codingcoyote <codingcoyote@duck.com>
2022-05-05 01:34:28 -0500

    get-wmiobject is deprecated
    replaced with get-diminstance

--- multipass/tools/chocolateyinstall.ps1
+++ multipass/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $pp = Get-PackageParameters
 if (!$pp['HyperVisor']) { $pp['HyperVisor'] = "Hyper-V" }
-$winversion = Get-WmiObject Win32_OperatingSystem | Select-Object Caption, Version, BuildNumber
+$winversion = Get-cimInstance -ClassName Win32_OperatingSystem | Select-Object Caption, Version, BuildNumber
 if ($pp['HyperVisor'] -match 'Hyper-*V') {
   if (((($winversion.Caption) -match '.*1[0-9]{1}.*Pro.*') -or (($winversion.Caption) -match '.*1[0-9]{1}.*Enterprise.*') -or (($winversion.Caption) -match '.*1[0-9]{1}.*Корпоративная.*')) -and ($winversion.BuildNumber -gt 17134)) {
     if ((Get-WindowsOptionalFeature -Online -FeatureName IIS-WebServer).State -eq "Disabled") {
