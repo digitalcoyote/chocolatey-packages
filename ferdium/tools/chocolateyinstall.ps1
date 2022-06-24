b419f889f8f6cf5863613c68c87ec57b71a2af98
codingcoyote <lokiphasma@gmail.com>
2022-06-23 20:09:29 -0500

    Added ferdium
    At the request of users and ferdium maintainers, ferdium was added

---
+++ ferdium/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$args = @{ 
+  PackageName = $env:ChocolateyPackageName
+  FileType = 'msi'
+  SilentArgs = '/qn'
+  Url64bit = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.74/Ferdium-6.0.0-nightly.74-ia32.msi'
+  Url = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.74/Ferdium-6.0.0-nightly.74-ia32.msi'
+  Checksum64 = '1ba983ec4d4ef83d8bf8ac4cd19ce7931c43fa4d358df5c4b133cf3f7d73137f9b9efcbd0b1b991a9907b918f7d28ae86cf3d9b2eec326d0e2b50cfaa4210676'
+  Checksum = '1ba983ec4d4ef83d8bf8ac4cd19ce7931c43fa4d358df5c4b133cf3f7d73137f9b9efcbd0b1b991a9907b918f7d28ae86cf3d9b2eec326d0e2b50cfaa4210676'
+  ChecksumType64 = 'sha512'
+  ChecksumType = 'sha512'
+}
+
+Install-ChocolateyPackage  @args
---
+++ ferdium/update.ps1
@@ -0,0 +1,32 @@
+﻿import-module au
+
+$releases = "https://github.com/ferdium/ferdium-app/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(Url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(Url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(ChecksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+      "(ChecksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/ferdium\/ferdium-app\/releases\/download\/v\d{1,4}\.\d{1,4}\.\d{1,4}.*\/Ferdium-\d{1,4}\.\d{1,4}\.\d{1,4}.*\.msi$'
+  $url64     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $url64 = "https://github.com$url64"
+  $version = $url64 -split '\/|v' | Select-Object -Last 1 -Skip 1
+  $url32 = "https://github.com/ferdium/ferdium-app/releases/download/v$version/Ferdium-$version-ia32.msi"
+  $releaseNotes = "https://github.com/ferdium/ferdium-app/releases/tag/v$version"
+  $versionParts = $version -split '-'
+  $versionParts[1] = $versionParts[1].replace('.', '-')  
+  $version = $versionParts -join '-'
+  return @{ Version = $version; URL64 = $url64; URL = $url32; ChecksumType64 = 'sha512'; ChecksumType32 = 'sha512'; ReleaseNotes = $releaseNotes}
+}
+
+Update-Package -ChecksumFor all
