ded932836657c46f595ff445f60cb33e0eb0152e
Chocolatey <chocolatey@realdimensions.net>
2022-06-24 00:07:12 +0000

    AU: 1 updated - deno [skip ci]

--- deno/tools/chocolateyinstall.ps1
+++ deno/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64 = 'https://github.com/denoland/deno/releases/download/v1.23.0/deno-x86_64-pc-windows-msvc.zip'
-$checksum64 = 'c864cf1a0db17c1337e9af200950788a98c2fd64e9e78a47c9020989ccb2bc1072f12ee2f40238ace86dab95abba49afb6a01a5a3ac2a85c399f88ca946b8b80'
+$url64 = 'https://github.com/denoland/deno/releases/download/v1.23.1/deno-x86_64-pc-windows-msvc.zip'
+$checksum64 = 'be295267b8cddb9d6ea0fd81a3b60a3e09b5e895a30fee5fb21f843ddc465d8b22d31f55de9152ab7658540b3872deaebb419ded6c1f4b5d41fcdc5c1d6f6541'
 $checksumType64 = 'sha512'
 $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
 Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -Url64 $url64 -UnzipLocation $UnzipLocation -CheckSum64 $checksum64 -CheckSumType64 $checksumType64
