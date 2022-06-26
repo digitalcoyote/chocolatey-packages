8d1b01d8bb9847dc045420a7c4e60ed90c8a9433
Chocolatey <chocolatey@realdimensions.net>
2022-06-26 16:06:30 +0000

    AU: 1 updated - ferdium [skip ci]

--- ferdium/tools/chocolateyinstall.ps1
+++ ferdium/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $args = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'msi'
   SilentArgs = '/qn'
-  Url64bit = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.77/Ferdium-6.0.0-nightly.77-ia32.msi'
-  Url = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.77/Ferdium-6.0.0-nightly.77-ia32.msi'
-  Checksum64 = '134bcb25c32aa07e157e53f82fe3e4ff8ce9f127590d7471d748a44ef9eca49dd2f04e7732f44126da6a9ed3ac4bc5ddac575bd434a2dbed352785aadc50b6a2'
-  Checksum = '134bcb25c32aa07e157e53f82fe3e4ff8ce9f127590d7471d748a44ef9eca49dd2f04e7732f44126da6a9ed3ac4bc5ddac575bd434a2dbed352785aadc50b6a2'
+  Url64bit = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.78/Ferdium-6.0.0-nightly.78-ia32.msi'
+  Url = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.78/Ferdium-6.0.0-nightly.78-ia32.msi'
+  Checksum64 = 'a589c4e1d29ae4dd5f1c68f6f9c210deeb9b31bf533a1b8544cda0ce188ad491100855a34d9c29869fc584999d13ef6e0baa6c8fb6150f9b6808172903fe704a'
+  Checksum = 'a589c4e1d29ae4dd5f1c68f6f9c210deeb9b31bf533a1b8544cda0ce188ad491100855a34d9c29869fc584999d13ef6e0baa6c8fb6150f9b6808172903fe704a'
   ChecksumType64 = 'sha512'
   ChecksumType = 'sha512'
 }
