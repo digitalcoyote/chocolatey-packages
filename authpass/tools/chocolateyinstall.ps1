82b1ec63b4974ebdb3b89fa67d6d64b65f29bf04
Chocolatey <chocolatey@realdimensions.net>
2022-06-30 00:07:00 +0000

    AU: 2 updated - authpass ferdium [skip ci]

--- authpass/tools/chocolateyinstall.ps1
+++ authpass/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $args = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'exe'
   SilentArgs = '/VERYSILENT'
-  Url64bit = 'https://github.com/authpass/authpass/releases/download/v1.9.6/AuthPass-setup-1.9.6_1904.exe'
-  Checksum64 = '77bc41632c72a9efd0d82f8ca44961de26f7294e77b3daba12d84bcef26c204737a28e0c6fcd9eb8d7cbdd1d8ed67ae0a49b443ba3f10340eda26d2946ba05d2'
+  Url64bit = 'https://github.com/authpass/authpass/releases/download/v1.9.7/AuthPass-setup-1.9.7_1940.exe'
+  Checksum64 = '945601ad0443291ce7198c756c26326022b5b3d3eb9fa86f9e06ba3d19c3930b70030d65d56a2b0c36b5d9bb46dfb55f48b861d7e14abf1ff39d0830a91dafb1'
   ChecksumType64 = 'sha512'
 }
 
