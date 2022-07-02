fdf3c6f8c96f595d5a04dfb4f2fd0e1692bfe099
codingcoyote <lokiphasma@gmail.com>
2022-07-02 15:40:10 -0500

    Fix #35 Update Ferdium to use EXE installer

--- ferdium/tools/chocolateyinstall.ps1
+++ ferdium/tools/chocolateyinstall.ps1
@@ -2,13 +2,10 @@
 
 $args = @{ 
   PackageName = $env:ChocolateyPackageName
-  FileType = 'msi'
-  SilentArgs = '/qn'
-  Url64bit = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.81/Ferdium-6.0.0-nightly.81.msi'
-  Url = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.81/Ferdium-6.0.0-nightly.81-ia32.msi'
-  Checksum64 = '781844be4ea5e8b16fc5a0cb58b78d77c0baf002321b08cb3daa4733f788e96215a5e64eb9832a9e6bc17c902a0976faa30cd6b4d89ace96d8056f4e1debf353'
-  Checksum = 'a563b358324575f81d3ee4f0d44e0d30440c500f9d1e39eb3750779e8c1cd430e75c05d60202d21d465d4269548ee959d59f8d9cd6795fe3daf5d412afe5a80f'
-  ChecksumType64 = 'sha512'
+  FileType = 'exe'
+  SilentArgs = '/ALLUSERS /S'
+  Url = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.85/Ferdium-win-AutoSetup-6.0.0-nightly.85.exe'
+  Checksum = 'edb481e20c1daaaf51789afc57594442cffcd50345b606e83a5a39f7563a64336647f87ba3932d3d37a0c6a8dad4598b6d6d83f6032a7359ef714691de4b6f5f'
   ChecksumType = 'sha512'
 }
 
--- ferdium/update.ps1
+++ ferdium/update.ps1
@@ -5,12 +5,9 @@ $releases = "https://github.com/ferdium/ferdium-app/releases"
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(Url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-      "(Url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-      "(Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-      "(Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-      "(ChecksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
-      "(ChecksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(Url\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(ChecksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
     }
   }
 }
@@ -20,13 +17,12 @@ function global:au_GetLatest {
   $regex   = '\/ferdium\/ferdium-app\/tree\/v\d{1,4}\.\d{1,4}\.\d{1,4}.*$'
   $url64     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
   $version = $url64 -split '\/|v' | Select-Object -Last 1
-  $url32 = "https://github.com/ferdium/ferdium-app/releases/download/v$version/Ferdium-win-installer-$version-ia32.msi"
-  $url64 = "https://github.com/ferdium/ferdium-app/releases/download/v$version/Ferdium-win-installer-$version-x64.msi"
+  $url64 = "https://github.com/ferdium/ferdium-app/releases/download/v$version/Ferdium-win-AutoSetup-$version.exe"
   $releaseNotes = "https://github.com/ferdium/ferdium-app/releases/tag/v$version"
   $versionParts = $version -split '-'
   $versionParts[1] = $versionParts[1].replace('.', '-')  
   $version = $versionParts -join '-'
-  return @{ Version = $version; URL64 = $url64; URL = $url32; ChecksumType64 = 'sha512'; ChecksumType32 = 'sha512'; ReleaseNotes = $releaseNotes}
+  return @{ Version = $version; URL64 = $url64; ChecksumType64 = 'sha512'; ReleaseNotes = $releaseNotes}
 }
 
-Update-Package -ChecksumFor all
+Update-Package -ChecksumFor all -Force
