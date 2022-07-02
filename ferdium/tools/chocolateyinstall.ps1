c8dca8fa58cac76781570c0ccae126dfd35b59a4
Chocolatey <chocolatey@realdimensions.net>
2022-07-02 20:29:01 +0000

    AU: 1 updated - ferdium [skip ci]

--- ferdium/tools/chocolateyinstall.ps1
+++ ferdium/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $args = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'msi'
   SilentArgs = '/qn'
-  Url64bit = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.81/Ferdium-6.0.0-nightly.81.msi'
-  Url = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.81/Ferdium-6.0.0-nightly.81-ia32.msi'
-  Checksum64 = '781844be4ea5e8b16fc5a0cb58b78d77c0baf002321b08cb3daa4733f788e96215a5e64eb9832a9e6bc17c902a0976faa30cd6b4d89ace96d8056f4e1debf353'
-  Checksum = 'a563b358324575f81d3ee4f0d44e0d30440c500f9d1e39eb3750779e8c1cd430e75c05d60202d21d465d4269548ee959d59f8d9cd6795fe3daf5d412afe5a80f'
+  Url64bit = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.85/Ferdium-win-installer-6.0.0-nightly.85-x64.msi'
+  Url = 'https://github.com/ferdium/ferdium-app/releases/download/v6.0.0-nightly.85/Ferdium-win-installer-6.0.0-nightly.85-ia32.msi'
+  Checksum64 = '2d47aee141f5418b67afa0d479fff39f4316eeea10ea7b70fd2ed2d75371b8739090f06cd3902ac2ae979dc71c00e369390f8081d126b9ad663f1ae6da7ac69f'
+  Checksum = '17c3f3353b333f2bec7746c8e681adbf79c7a879c804e1548c73f1859226d745c3e5558ce907385520bb565407c54110db771b0969088f05365df3b8dc33511b'
   ChecksumType64 = 'sha512'
   ChecksumType = 'sha512'
 }
