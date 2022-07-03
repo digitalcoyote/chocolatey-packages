4c56d3a34cca9fb3fa0bc7968afb4bb586c1238a
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-07-03 22:06:21 +0800

    Fix: Update Uninstall Path

--- mypaint/tools/chocolateyuninstall.ps1
+++ mypaint/tools/chocolateyuninstall.ps1
@@ -1,3 +1,3 @@
 ﻿$bit = (Get-WMIObject Win32_Processor).AddressWidth | Select-Object -First 1
-$file = "$Env:Programfiles\mypaint-git-w$bit\uninstall.exe"
+$file = "$Env:Programfiles\mypaint-w$bit\uninstall.exe"
 Uninstall-ChocolateyPackage -PackageName $env:ChocolateyPackageName -File $file -FileType 'exe' -SilentArgs '/S'
