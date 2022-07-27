8450d8b442da91836ac35ff2f657fa05e39950e8
Chocolatey <chocolatey@realdimensions.net>
2022-07-27 16:06:56 +0000

    AU: 2 updated - nethermind oh-my-posh [skip ci]

--- nethermind/tools/chocolateyinstall.ps1
+++ nethermind/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $ZipInstallArgs = @{
     PackageName = $env:ChocolateyPackageName 
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = 'https://github.com/NethermindEth/nethermind/releases/download/1.13.4/nethermind-windows-amd64-1.13.4-3e5972c-20220630.zip'
-    CheckSum64 = 'be3d179b08b12e405b6a49ace8b370e643b0dabfa6cfbf067c1122aa1c338adae0a6eeb66c04dca4adec86d1322523fc9ccbfe82f8683f3e181b613421074622'
+    Url64 = 'https://github.com/NethermindEth/nethermind/releases/download/1.13.5/nethermind-windows-amd64-1.13.5-c6b8a24-20220727.zip'
+    CheckSum64 = 'cccf25ced77d6b1b19bceeade56f7a6ae81115611f8be3eb8bc3be4b002662e89375986782725950a40705b364f05807c151a40135367637c67296453c759ffe'
     CheckSumType64 = 'sha512'
 }
 
