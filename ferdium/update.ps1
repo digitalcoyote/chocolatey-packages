dd7aff2e0dba35fcef00b03fc93dbc22cddc5fa5
codingcoyote <lokiphasma@gmail.com>
2022-07-28 18:47:08 -0500

    rework ferdium regex to only look for beta and stable releases

--- ferdium/update.ps1
+++ ferdium/update.ps1
@@ -14,15 +14,18 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $regex   = '\/ferdium\/ferdium-app\/tree\/v\d{1,4}\.\d{1,4}\.\d{1,4}.*$'
+  $regex   = '\/ferdium\/ferdium-app\/tree\/v\d{1,4}\.\d{1,4}\.\d{1,4}(?:\.\d{1,4})?(?:-beta.*)?$'
   $url64     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
   $version = $url64 -split '\/|v' | Select-Object -Last 1
   $url64 = "https://github.com/ferdium/ferdium-app/releases/download/v$version/Ferdium-win-AutoSetup-$version.exe"
   $releaseNotes = "https://github.com/ferdium/ferdium-app/releases/tag/v$version"
   $versionParts = $version -split '-'
-  $versionParts[1] = $versionParts[1].replace('.', '-')  
-  $version = $versionParts -join '-'
+  if ($versionParts.Length -gt 1) {
+    $versionParts[1] = $versionParts[1].replace('.', '-')  
+    $version = $versionParts -join '-'  
+  }
+
   return @{ Version = $version; URL64 = $url64; ChecksumType64 = 'sha512'; ReleaseNotes = $releaseNotes}
 }
 
-Update-Package -ChecksumFor all -Force
+Update-Package -ChecksumFor all
