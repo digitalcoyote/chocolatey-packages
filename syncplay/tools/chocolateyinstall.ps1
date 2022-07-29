dff35d7aead1578c3fd0ce4875e5fdb2267c5ffe
codingcoyote <lokiphasma@gmail.com>
2022-07-29 12:38:46 -0500

    Correcting syncplay package
    correcting nuspec and install scripts.
    Allowing pre-release versions

--- syncplay/tools/chocolateyinstall.ps1
+++ syncplay/tools/chocolateyinstall.ps1
@@ -1,12 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
-if(!$pp['Lang']) {$pp['Site'] = "Default Web Site"}
+$Lang = "1033"
+if ($pp.count -gt 0 -and $pp['Lang']) { $Lang = $pp['Lang'] }
 
 $installArgs = @{ 
-  PackageName = $env:ChocolateyPackageName
-  FileType = 'EXE'
-  Url64bit = 'https://github.com/Syncplay/syncplay/releases/download/v1.6.9/Syncplay-1.6.9-Setup.exe'
-  Checksum64 = '14b5845adadc6bb909c0f60369ee1f4e751733d561a39ab59ed1c70a63bd82e8b4f6cb05245976279d81f135de854f93318e4cc81fb262c3d0f9444c040222ce'
-  SilentArgs = "/S /LANG=$($pp['Lang'])"
+  PackageName    = $env:ChocolateyPackageName
+  FileType       = 'EXE'
+  Url64bit       = 'https://github.com/Syncplay/syncplay/releases/download/v1.7.0-persistentrooms1/Syncplay-1.7.0-PersistetRoomsTest1-Setup.exe'
+  Checksum64     = '908d96c8e892ca3fbc1540b0e677e472ad20517e7be11adc85ae82898e68d252788356db82e1d012d48f412cb9f9f147e483bca6eb94def5839b6f60d8ed11b1'
+  SilentArgs     = "/S /LANG=$($Lang)"
   ChecksumType64 = 'sha512'
 }
 
