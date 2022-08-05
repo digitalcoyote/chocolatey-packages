8a9335a6126d3d6d27a6302354ec203efa1cffea
Chocolatey <chocolatey@realdimensions.net>
2022-08-05 08:06:52 +0000

    AU: 2 updated - jcli sidequest [skip ci]

--- jcli/tools/chocolateyinstall.ps1
+++ jcli/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:Chocolat
 $ChocolateyInstallZipArguments = @{
     PackageName = $env:ChocolateyPackageName
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = 'https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.38/jcli-windows-amd64.zip'
-    CheckSum64 = 'a8ab2bd5af227db7bd55ac7ecd751b0c9c22ee9221bbd48624e111b0920ca669c59b5d6a2eb2168d231319beed1aa75f65b63946889b763d6a0dbab30fb6188e'
+    Url64 = 'https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.39/jcli-windows-amd64.zip'
+    CheckSum64 = 'e3161e180099bc9c9f405bd2834852571328a3031f7094f8727233ab7620a2dd7638548622ab02c93307ef9b5c9ffab12dbac64d60ae3f5fb973aa3dea14aad9'
     CheckSumType64 = 'sha512'
 }
 
--- sidequest/tools/chocolateyinstall.ps1
+++ sidequest/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileType = 'exe'
     SilentArgs = '/S'
-    URL64 = 'https://github.com/SideQuestVR/SideQuest/releases/download/v0.10.28/SideQuest-Setup-0.10.28-x64-win.exe'
-    Checksum64 = 'ca5be9be4b22357348fc064c8ed3fbd85f08096ca8908641f463176bd0b1e03a23bf1dfb056a5e6644e6dd033777f9698131211a79f966ef42ab8b41b9408c44'
+    URL64 = 'https://github.com/SideQuestVR/SideQuest/releases/download/v0.10.30/SideQuest-Setup-0.10.30-x64-win.exe'
+    Checksum64 = '0421eb3c23fef86c4ca03210892edbb31ee77b865f9a2107468b73b379cb6985fb9cf28dfa406726cc8b274b290687837ec95ad8b65fe28ed275592d37cbea8e'
     ChecksumType64 = 'sha512'
 }
 
