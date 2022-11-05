58b4d07de325fa0da63529d090745e83768e9bee
codingcoyote <codingcoyote@duck.com>
2022-11-05 15:34:35 -0500

    Removing Shim Generation from Deno

--- deno/tools/chocolateyinstall.ps1
+++ deno/tools/chocolateyinstall.ps1
@@ -4,5 +4,15 @@ $url64 = 'https://github.com/denoland/deno/releases/download/v1.27.1/deno-x86_64
 $checksum64 = '7e0680329eeb58e29cfd06f6d21af4668769b0b0f0fa68f7797fe44e86e977f382e9c29f98235f9ceb8e73ff57c9e45c623988c69c0eafb7ec2bddfbb02c7f64'
 $checksumType64 = 'sha512'
 $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
+
+
 Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -Url64 $url64 -UnzipLocation $UnzipLocation -CheckSum64 $checksum64 -CheckSumType64 $checksumType64
+
+#Create .ignore files so chocolatey does not shim the Exe
+$files = get-childitem $toolsDir -include *.exe -recurse
+foreach ($file in $files) {
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
+Install-ChocolateyPath -PathToInstall $UnzipLocation
+
 Write-Output "Run 'deno --help' to get started"
