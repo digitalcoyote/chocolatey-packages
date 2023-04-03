b8c5013d52a1732b9da86460c3fc51b31dae062e
Curtis Carter <coyote@duck.com>
2023-04-03 08:01:42 -0700

    Fix bad find-replace (version -<> vertestn) in milkman-test

--- milkman-test/update.ps1
+++ milkman-test/update.ps1
@@ -1,7 +1,7 @@
 ﻿import-module au
 
 $releases = "https://github.com/warmuuh/milkman/releases"
-$PackageName = "milkman-scripting"
+$PackageName = "milkman-test"
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
@@ -9,7 +9,7 @@ function global:au_SearchReplace {
       "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
     }
     "$PackageName.nuspec" = @{
-      "(\<dependency id=""milkman"" version=""\[).*(\]""\s*/>)" = "`${1}$($Latest.Version)`$2"
+      "(\<dependency id=""milkman"" Version=""\[).*(\]""\s*/>)" = "`${1}$($Latest.Version)`$2"
     }
   }
 }
@@ -23,8 +23,8 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
   $regex   = '\/warmuuh\/milkman\/releases\/tag\/[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$'
   $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
-  $version = $url -split '\/' | Select-Object -Last 1
-  $url = 'https://github.com/warmuuh/milkman/releases/download/' + $version + '/milkman-dist-plugins-bin.zip'
+  $Version = $url -split '\/' | Select-Object -Last 1
+  $url = 'https://github.com/warmuuh/milkman/releases/download/' + $Version + '/milkman-dist-plugins-bin.zip'
   $DownloadedFile = "$PSScriptRoot\milkman-dist-plugins-bin.zip"
   (New-Object System.Net.WebClient).DownloadFile($url, $DownloadedFile)
   Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/warmuuh/milkman/master/LICENSE' -OutFile 'tools\License.txt'
@@ -33,7 +33,7 @@ function global:au_GetLatest {
   Get-ChildItem -Path $UnzipDirectory  -Filter "$PackageName.jar" -Recurse | Select-Object -first 1 | Copy-Item -Destination "tools\$PackageName.jar"
   Remove-Item -Path "milkman-dist-plugins-bin.zip"
   Remove-Item -Path "milkman-dist-plugins-bin" -Recurse
-  return @{ Version = $version;}
+  return @{ Version = $Version;}
 }
 
 Update-Package -ChecksumFor none
