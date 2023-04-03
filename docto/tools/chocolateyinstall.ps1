91bebb2cb9021b4182f3083111e1c741bebfbc70
Curtis Carter <coyote@duck.com>
2023-04-03 11:12:42 -0700

    Fix docto updates

--- docto/tools/chocolateyinstall.ps1
+++ docto/tools/chocolateyinstall.ps1
@@ -2,10 +2,13 @@
 
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
-    URL = 'https://github.com/tobya/DocTo/releases/download/V1.5/docto105.zip'
+    URL = ''
+    URL32 = ''
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Checksum = '872558d1b220824d4ca2155de56694211b1b62441a8a7286fc5df0f0b0e6abc08ea505ecf8a3e7fe15eacb4fbfdb62a036508f4d7c531e459b17524ad2cb1dca'
+    Checksum = ''
+    Checksum32 = ''
     ChecksumType = 'sha512'
+    ChecksumType32 = 'sha512'
 }
 
 
--- docto/update.ps1
+++ docto/update.ps1
@@ -5,21 +5,25 @@ $releases = "https://github.com/tobya/DocTo/releases/"
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
+      "(URL32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
       "(URL\s*=\s*)('.*')" = "`$1'$($Latest.URL)'"
-      "(Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-      "(ChecksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(Checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+      "(ChecksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(ChecksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType)'"
     }
   }
 }
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $regex   = '/tobya/DocTo/tree/[vV][0-9]{1,4}\.{0,1}[0-9]{0,4}\.{0,1}[0-9]{0,4}.*'
+  $regex   = '/tobya/DocTo/tree/[vV]\d{1,4}\.{0,1}\d{0,4}\.{0,1}\d{0,4}.*'
   $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
   $version = $url -split '\/|V' | Select-Object -Last 1
-  $url = "https://github.com/tobya/DocTo/releases/download/V$version/docto$($version.Replace('.', '0')).zip"
+  $url = "https://github.com/tobya/DocTo/releases/download/v$version/docto_32.zip"
+  $url64 = "https://github.com/tobya/DocTo/releases/download/v$version/docto_64.zip"
 
-  return @{ Version = $version; URL = $url; ChecksumType32 = 'sha512';}
+  return @{ Version = $version; URL = $url; URL64= $url64; ChecksumType32 = 'sha512'; ChecksumType = 'sha512';}
 }
 
 Update-Package -ChecksumFor 32
