b9da902235892bf940408f527768378e75340337
Curtis Carter <coyote@duck.com>
2023-04-03 07:25:37 -0700

    Added milkman-auth
    Added the auth plugin for milkman

---
+++ milkman-auth/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$checksum64 = '5B7553C39BC63306C3B651B84B0A16B593D3B47BE79C5753A37E83B92586E5267328469E12F7CED7E9ED4C2CAC8CD53505B2D22D8CFC330329027414A8D43D70'
+$checksumType64 = 'sha512'
+$JarFile = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path (Join-Path 'lib' $env:ChocolateyPackageName) 'tools')) "$env:ChocolateyPackageName.jar"
+Get-ChecksumValid -File $JarFile -CheckSum $checksum64 -CheckSumType $checksumType64
+$InstallLocation = (Join-Path (Join-Path (Join-Path (Get-ToolsLocation) "milkman") "plugins") "$env:ChocolateyPackageName.jar")
+Copy-Item -Path $JarFile -Destination $InstallLocation -Recurse
+Remove-Item -Path $JarFile -Force
+Write-Host "Installed $($env:ChocolateyPackageName) to $($InstallLocation)"
