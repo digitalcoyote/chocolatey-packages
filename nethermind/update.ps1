7d673bda187648bef8fa689b5c9b85c03fea154f
Curtis Carter <coyote@duck.com>
2023-04-03 10:53:54 -0700

    Fix nethermind update

--- nethermind/update.ps1
+++ nethermind/update.ps1
@@ -11,13 +11,17 @@ function global:au_SearchReplace {
     }
   }
 }
-
+https://github.com/NethermindEth/nethermind/releases/download/1.17.3/nethermind-1.17.3-671d60ad-windows-x64.zip
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $regex   = '\/NethermindEth\/nethermind\/releases\/download\/\d{1,3}\.\d{1,3}\.\d{1,3}.*\/nethermind-windows-amd64-\d{1,3}\.\d{1,3}\.\d{1,3}.*\.zip$'
+  $regex   = '\/NethermindEth\/nethermind\/tree\/\d{1,3}\.\d{1,3}\.\d{1,3}\.*\d*'
   $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
-  $version = $url -split '\/' | Select-Object -Last 1 -Skip 1
-  $url = "https://github.com$url"
+  $version = $url -split '\/' | Select-Object -Last 1
+
+  $versionReleasePage = Invoke-WebRequest -Uri "https://github.com/NethermindEth/nethermind/releases/tag/$version" -UseBasicParsing
+  $downloadregex      = "\/NethermindEth\/nethermind\/releases\/download\/$version\/neterind-$version-.*-windows-x64.zip"
+  $url                = $versionReleasePage.links | Where-Object href -match $downloadregex | Select-Object -First 1 -expand href
+  $url                = "https://github.com$url"
   return @{ Version = $version; URL64 = $url; ChecksumType64 = 'sha512';}
 }
 
