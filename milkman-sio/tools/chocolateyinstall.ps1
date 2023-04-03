c32a18e466013e2804325035ea886397c0653400
Curtis Carter <coyote@duck.com>
2023-04-03 07:32:17 -0700

    Added milkman-sio
    Adedd the MilkMan Socket.IO plugin

---
+++ milkman-sio/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$checksum64 = '5084994CD7A39F20DDD93B5737C01BBD5BD0C3DE198676183754CC203CB0B86F5E265C23CD2BF38D3C9226202EB61C0F03431399F6BA5F77D430F8BC1C7AE352'
+$checksumType64 = 'sha512'
+$JarFile = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path (Join-Path 'lib' $env:ChocolateyPackageName) 'tools')) "$env:ChocolateyPackageName.jar"
+Get-ChecksumValid -File $JarFile -CheckSum $checksum64 -CheckSumType $checksumType64
+$InstallLocation = (Join-Path (Join-Path (Join-Path (Get-ToolsLocation) "milkman") "plugins") "$env:ChocolateyPackageName.jar")
+Copy-Item -Path $JarFile -Destination $InstallLocation -Recurse
+Remove-Item -Path $JarFile -Force
+Write-Host "Installed $($env:ChocolateyPackageName) to $($InstallLocation)"
