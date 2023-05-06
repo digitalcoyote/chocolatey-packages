c9b856d303b27ab6491b8adc64baaf45d217c805
Curtis Carter <lokiphasma@gmail.com>
2023-05-06 13:08:37 -0500

    Update chocolateyinstall.ps1
    Fix #46 Supporting Windows Edition checks in another non-english language

--- multipass/tools/chocolateyinstall.ps1
+++ multipass/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $pp = Get-PackageParameters
 if (!$pp['HyperVisor']) { $pp['HyperVisor'] = "Hyper-V" }
 $winversion = Get-cimInstance -ClassName Win32_OperatingSystem | Select-Object Caption, Version, BuildNumber
 if ($pp['HyperVisor'] -match 'Hyper-*V') {
-  if (((($winversion.Caption) -match '.*1[0-9]{1}.*Pro.*') -or (($winversion.Caption) -match '.*1[0-9]{1}.*Enterprise.*') -or (($winversion.Caption) -match '.*1[0-9]{1}.*Корпоративная.*')) -and ($winversion.BuildNumber -gt 17134)) {
+  if (((($winversion.Caption) -match '.*1[0-9]{1}.*Pro.*') -or (($winversion.Caption) -match '.*1[0-9]{1}.*Enterprise.*') -or (($winversion.Caption) -match '.*1[0-9]{1}.*专业.*')) -or (($winversion.Caption) -match '.*1[0-9]{1}.*企业.*')) -or (($winversion.Caption) -match '.*1[0-9]{1}.*Корпоративная.*')) -and ($winversion.BuildNumber -gt 17134)) {
     if ((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V).State -eq "Disabled") {
       Write-Output "Installing Hyper-V"
       Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
