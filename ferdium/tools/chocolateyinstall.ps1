ea66c01c9cf47630b26339b8f661de2bc5f1b9aa
Chocolatey <chocolatey@realdimensions.net>
2023-08-11 08:07:30 +0000

    AU: 1 updated - ferdium [skip ci]

--- ferdium/tools/chocolateyinstall.ps1
+++ ferdium/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $args = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'exe'
   SilentArgs = '/ALLUSERS /S'
-  Url = 'https://github.com/ferdium/ferdium-app/releases/download/v6.4.1-beta.1/Ferdium-win-AutoSetup-6.4.1-beta.1.exe'
-  Checksum = '52d8b1d733108fc3af72d42bc8027264463147cd4d246395c1b051b2354606302d27f29114deabdc64e8c9a23ae30f772d989814090dd14285e34f3542074c53'
+  Url = 'https://github.com/ferdium/ferdium-app/releases/download/v6.4.1-beta.2/Ferdium-win-AutoSetup-6.4.1-beta.2.exe'
+  Checksum = 'cb752a704b4fce6f3e57ddc9a4f8e926f3ee6513c87e21dc4d1ae8d0e25cb82db6356cda1fc91b9cdbebdbb6ce5a584931a6134e40431089a52ce9ee861def46'
   ChecksumType = 'sha512'
 }
 
