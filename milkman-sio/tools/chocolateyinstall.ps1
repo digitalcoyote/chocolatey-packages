282d7c40cf1af8d0b05e20df7f9877e407ea809a
Chocolatey <chocolatey@realdimensions.net>
2024-01-30 04:27:30 +0000

    AU: 1 updated - milkman-sio [skip ci]

--- milkman-sio/tools/chocolateyinstall.ps1
+++ milkman-sio/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$checksum64 = '5084994CD7A39F20DDD93B5737C01BBD5BD0C3DE198676183754CC203CB0B86F5E265C23CD2BF38D3C9226202EB61C0F03431399F6BA5F77D430F8BC1C7AE352'
+$checksum64 = 'B1F996CA6E5C58AA94015824AC2C118D058317743C4533CB151BB4A39342E7DABD6856CFCBC61FA1A313988F0FF04B1F3671106F1560C1DF024C84A00A185366'
 $checksumType64 = 'sha512'
 $JarFile = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path (Join-Path 'lib' $env:ChocolateyPackageName) 'tools')) "$env:ChocolateyPackageName.jar"
 Get-ChecksumValid -File $JarFile -CheckSum $checksum64 -CheckSumType $checksumType64
