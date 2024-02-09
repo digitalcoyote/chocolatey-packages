3716ac5d25146f03107e16dbd416702e065a7aae
Chocolatey <chocolatey@realdimensions.net>
2024-02-09 16:08:16 +0000

    AU: 1 updated - docto [skip ci]

--- docto/tools/chocolateyinstall.ps1
+++ docto/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $InstallArgs = @{
     PackageName    = $env:ChocolateyPackageName
     UnzipLocation  = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    URL64          = 'https://github.com/tobya/DocTo/releases/download/v1.9/docto_64.zip'
-    URL            = 'https://github.com/tobya/DocTo/releases/download/v1.9/docto_32.zip'
-    Checksum64     = '35a31d6791df113b41943278c08cdef16c16435a2dfbff6400429284fa447dd4288109533a70182396bd122aed774bcf0fbcdcf50b9e1f9091e5bb62586f408c'
-    Checksum       = '831d7280386d95bf84629b8eb596bad7cbb3cc266aea7691767b11dae6a2014c'
+    URL64          = 'https://github.com/tobya/DocTo/releases/download/v1.11/docto_64.zip'
+    URL            = 'https://github.com/tobya/DocTo/releases/download/v1.11/docto_32.zip'
+    Checksum64     = '893a951a27dd619938b934546a0f7328c677131185dc45e22e4acae7109df3e14be319c16556ede37457f09c3a27aec037c3fff283b701c93fd38858a8231244'
+    Checksum       = '2fbced131bef3ad02ff97475769931f62f12b8c4b70700fd6e9be5f9cf946571'
     ChecksumType64 = 'sha512'
     ChecksumType   = 'sha512'
 }
