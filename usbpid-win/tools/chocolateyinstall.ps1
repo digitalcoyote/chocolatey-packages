1dea7f2b0069f8bc6d0e7f58b544afcb1c1140de
Chocolatey <chocolatey@realdimensions.net>
2024-02-17 21:58:13 +0000

    AU: 1 updated - usbpid-win [skip ci]

--- usbpid-win/tools/chocolateyinstall.ps1
+++ usbpid-win/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileType = 'msi'
     SilentArgs = '/qn'
-    URL = ''
-    Checksum = ''
+    URL = 'https://github.com/dorssel/usbipd-win/releases/download/v4.1.0/usbipd-win_4.1.0.msi'
+    Checksum = '6bf8b5249d5f322dfd90ae26320d7e9f9c9dc4ea8b6dbd5b62ad35a81224b8823a0713a0779f6d675a394f1d3edcf505983b6f46a493cf40102bfcb189bc0f44'
     ChecksumType = 'sha512'
 }
 
