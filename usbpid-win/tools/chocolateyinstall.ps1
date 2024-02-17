4482b6f187ef1ec8913f8f4a34d238f35b4cf73e
codingcoyote <lokiphasma@gmail.com>
2024-02-17 15:54:53 -0800

    Add usbpid-win package

---
+++ usbpid-win/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$InstallArgs = @{
+    PackageName = $env:ChocolateyPackageName
+    FileType = 'msi'
+    SilentArgs = '/qn'
+    URL = ''
+    Checksum = ''
+    ChecksumType = 'sha512'
+}
+
+Install-ChocolateyPackage @InstallArgs
+
---
+++ usbpid-win/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+
+$releases = "https://github.com/dorssel/usbipd-win/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(URL\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(ChecksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/dorssel\/usbipd-win\/releases\/tag\/v[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/|v' | Select-Object -Last 1
+  $url = "https://github.com/dorssel/usbipd-win/releases/download/v$version/usbipd-win_$version.msi"
+  return @{ Version = $version; URL64 = $url; ChecksumType64 = 'sha512';}
+}
+
+Update-Package -ChecksumFor 64
