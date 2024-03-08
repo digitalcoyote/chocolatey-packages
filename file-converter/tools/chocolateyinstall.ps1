a15d735d134260cf175fccf2400e2ad2b60f7955
Chocolatey <chocolatey@realdimensions.net>
2024-03-08 00:41:03 +0000

    AU: 1 updated - file-converter [skip ci]

--- file-converter/tools/chocolateyinstall.ps1
+++ file-converter/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $InstallArgs = @{
     PackageName    = $env:ChocolateyPackageName
     FileType       = 'msi'
     SilentArgs     = '/q'
-    URL          = 'https://github.com/Tichau/FileConverter/releases/download/v1.2.3/FileConverter-1.2.3-x64-setup.msi'
-    Checksum     = '651fddb9e4f6250a29a31e7a152217b196ca7642de0e6c9a4b4fd1ef0f83a769bc9c22bd07ef7310b104ee1171c37460be99daf0fd08d2d415d06b8df78d62ee'
+    URL          = 'https://github.com/Tichau/FileConverter/releases/download/v2.0/FileConverter-2.0-x64-setup.msi'
+    Checksum     = 'd934f9adc34e8dbebc538e84d9958aa8dc6635f2a2601ffc974a3394a31134e4bdd169bf7296d12dacb1cebc7fe058ad7611c4be9a0827924f09b65f37f418d2'
     ChecksumType = 'sha512'
 }
 
