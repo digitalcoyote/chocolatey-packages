bbe0e7ff3ae656b1d640821b5e9f2fed69017046
Curtis Carter <lokiphasma@gmail.com>
2024-03-08 18:56:46 -0800

    Fix Android log viewer update script

--- android-log-viewer/update.ps1
+++ android-log-viewer/update.ps1
@@ -14,10 +14,9 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $regex   = "\/MarkusPalcer\/AndroidLogViewer\/releases\/download\/v\d{1,3}\.\d{1,3}\.\d{1,3}\/Android.Log.Viewer.msi$"
+  $regex   = "\/MarkusPalcer\/AndroidLogViewer\/tree\/v\d{1,3}\.\d{1,3}\.\d{1,3}"
   $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
-  $version = $url -split '\/' | Select-Object -Last 1 -Skip 1
-  $version = $version -split 'v' | Select-Object -Last 1
+  $version = $url -split '\/|v' | Select-Object -Last 1
   $url = "https://github.com/MarkusPalcer/AndroidLogViewer/releases/download/v$version/Android.Log.Viewer.msi"
   return @{ Version = $version; URL = $url; ChecksumType32 = 'sha512';}
 }
