670603f2e9c9828c25793f26da5f96e562d36e7e
Chocolatey <chocolatey@realdimensions.net>
2024-04-06 00:08:33 +0000

    AU: 1 updated - syncplay [skip ci]

--- syncplay/tools/chocolateyinstall.ps1
+++ syncplay/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ if ($pp.count -gt 0 -and $pp['Lang']) { $Lang = $pp['Lang'] }
 $installArgs = @{ 
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'EXE'
-  Url64bit       = 'https://github.com/Syncplay/syncplay/releases/download/v1.7.1/Syncplay-1.7.1-Setup.exe'
-  Checksum64     = 'd79897727d5a5aa36889f94c30b76e3ba4cd25b7e4fe9f8c8183410e2d93636d63f8c49fbe9ac76482be68e997e6acde1c621f5b8a0280e2d8961992485f08e5'
+  Url64bit       = 'https://github.com/Syncplay/syncplay/releases/download/v1.7.2/Syncplay-1.7.2-Setup.exe'
+  Checksum64     = '9eb3982c40ed5ea85ae1cad7654890b0a6e6efccfc15de7205a165824ab86f2a575fa38b38b9b0d6a9c13d4f8ab80caea31f2227e2db2efbe0828afaa38c2df6'
   SilentArgs     = "/S /LANG=$($Lang)"
   ChecksumType64 = 'sha512'
 }
