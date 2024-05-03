5f44d25d4095e02fa33c7493e5202fa422fdf98f
Chocolatey <chocolatey@realdimensions.net>
2024-05-03 00:08:24 +0000

    AU: 1 updated - sidequest [skip ci]

--- sidequest/tools/chocolateyinstall.ps1
+++ sidequest/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileType = 'exe'
     SilentArgs = '/S'
-    URL64 = 'https://github.com/SideQuestVR/SideQuest/releases/download/v0.10.41/SideQuest-Setup-0.10.41-x64-win.exe'
-    Checksum64 = '0757a774f9108376a1fc113d3bc6f461fb8b7f9f978f745830fd24ec88a44e08553d398f1a7774b17bce302de7851129fe996a6881fd3cdd75c54dc018ed39a0'
+    URL64 = 'https://github.com/SideQuestVR/SideQuest/releases/download/v0.10.42/SideQuest-Setup-0.10.42-x64-win.exe'
+    Checksum64 = 'c75cf641590eaa8cd17bd50fed30e7d4359c5f7840957ee4bf692e01af4741029f471217df1716a3266f6c66ab446410fc84f9d011e591566bbf46555320f177'
     ChecksumType64 = 'sha512'
 }
 
