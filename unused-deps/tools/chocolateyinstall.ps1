7a546d05da066a53503b83d5a3756775b1ba9136
Chocolatey <chocolatey@realdimensions.net>
2024-05-23 16:08:31 +0000

    AU: 1 updated - unused-deps [skip ci]

--- unused-deps/tools/chocolateyinstall.ps1
+++ unused-deps/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
-    URL64 = 'https://github.com/bazelbuild/buildtools/releases/download/v7.1.1/unused_deps-windows-amd64.exe'
+    URL64 = 'https://github.com/bazelbuild/buildtools/releases/download/v7.1.2/unused_deps-windows-amd64.exe'
     FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'unused_deps.exe'
-    Checksum64 = '6b0153b1114c33cbb9bc332737bb9caa122eb46e74645bf8f9e458a26dd0885e0d69480b0fea8cc6e2550276e784937d419f2fa6de08a5351aafb660ec84a800'
+    Checksum64 = '93b43eafbd1e4ed23be0b8261dba1db39c210e269bff4ee81a1cba3373eb78b3365ae8490855821c9467600ac67378d0b4e7c4bdc0b4692a9c56498db8b38b65'
     ChecksumType64 = 'sha512'
 }
 
