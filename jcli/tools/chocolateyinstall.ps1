407857f10bdbede702f264dde04d3e9e680bd772
Chocolatey <chocolatey@realdimensions.net>
2024-12-25 08:09:22 +0000

    AU: 1 updated - jcli [skip ci]

--- jcli/tools/chocolateyinstall.ps1
+++ jcli/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:Chocolat
 $ChocolateyInstallZipArguments = @{
     PackageName = $env:ChocolateyPackageName
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = 'https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.44/jcli-windows-amd64.zip'
-    CheckSum64 = '53610b01014fa8cd93dd29a28e23533e10093e92ad1d8840e441c8e33b77dfc5502d0266fb15efb0e3440ae9bf7d9235c27bb34be50c0f185c9ebf3722156a05'
+    Url64 = 'https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.45/jcli-windows-amd64.zip'
+    CheckSum64 = '1d436fe7c4e262dddcca6cd36140927b814250afb8bafc689e32ef7007299b415cef586f8910a1ccba53ae784780b36db0a59df0ba5560793ef74799bfb684d9'
     CheckSumType64 = 'sha512'
 }
 
