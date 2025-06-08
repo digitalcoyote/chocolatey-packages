30e4b4196c546d031307e1157a491c2252f6c04c
Curtis Carter <lokiphasma@gmail.com>
2025-06-08 16:26:25 -0700

    Update usbipd-win

--- usbipd-win/tools/chocolateyinstall.ps1
+++ usbipd-win/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileType = 'msi'
     SilentArgs = '/qn'
-    URL = 'https://github.com/dorssel/usbipd-win/releases/download/v4.1.0/usbipd-win_4.1.0.msi'
-    Checksum = '6bf8b5249d5f322dfd90ae26320d7e9f9c9dc4ea8b6dbd5b62ad35a81224b8823a0713a0779f6d675a394f1d3edcf505983b6f46a493cf40102bfcb189bc0f44'
+    URL = 'https://github.com/dorssel/usbipd-win/releases/download/v5.1.0/usbipd-win_5.1.0_x64.msi'
+    Checksum = '38f6dafb332171d7194bdd8af29afe9cbaa67aab427ced4dc9350c116f828c17f2db82c7170bdd1850165f8d0a2c198f2c2214c6f96ee1dddee45d72fe136686'
     ChecksumType = 'sha512'
 }
 
