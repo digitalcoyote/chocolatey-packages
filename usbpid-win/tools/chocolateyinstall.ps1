2c0ce9cb47c23a8701dd73447a473d65504ea8e4
Curtis Carter <lokiphasma@gmail.com>
2025-07-10 07:55:08 -0700

    Wrong package name for usbpid-win

--- usbpid-win/tools/chocolateyinstall.ps1
+++ usbpid-win/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $InstallArgs = @{
     FileType = 'msi'
     SilentArgs = '/qn'
     URL = 'https://github.com/dorssel/usbipd-win/releases/download/v5.1.0/usbipd-win_5.1.0_x64.msi'
-    Checksum = '38f6dafb332171d7194bdd8af29afe9cbaa67aab427ced4dc9350c116f828c17f2db82c7170bdd1850165f8d0a2c198f2c2214c6f96ee1dddee45d72fe136686'
+    Checksum = ''
     ChecksumType = 'sha512'
 }
 
