a445ed567e0fe75e148e6be0fd8cedd984a646c2
Chocolatey <chocolatey@realdimensions.net>
2026-03-21 00:10:28 +0000

    AU: 2 updated - kubectx kubens [skip ci]

--- kubectx/tools/chocolateyinstall.ps1
+++ kubectx/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $ZipInstallArgs = @{
     PackageName = $env:ChocolateyPackageName 
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = 'https://github.com/ahmetb/kubectx/releases/download/v0.9.5/kubectx_v0.9.5_windows_x86_64.zip'
-    CheckSum64 = '855e3b7fb57a5f3bc9d58d2c1b46fe9a929e2baf79d4065f5a7088627b7f1629deb1624425969d4f2c25baf12a89da605b1a6893e69ef009ed31a52468a768a6'
+    Url64 = 'https://github.com/ahmetb/kubectx/releases/download/v0.10.0/kubectx_v0.10.0_windows_x86_64.zip'
+    CheckSum64 = 'd2db1aa37148cc28774277a46025fb3c0b719a3eb5ab78c143d13cf9dc4fc86cf10903d374a8420ab2e605fcd74deb9cb6e483cb766736045111e249fa6563f1'
     CheckSumType64 = 'sha512'
 }
 
--- kubens/tools/chocolateyinstall.ps1
+++ kubens/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $ZipInstallArgs = @{
     PackageName = $env:ChocolateyPackageName 
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = 'https://github.com/ahmetb/kubectx/releases/download/v0.9.5/kubens_v0.9.5_windows_x86_64.zip'
-    CheckSum64 = 'b49b608b63ae896bb3529b3074d8b228b1691747138888857e7e9c184b68b81cf3e0c1a32b29489a80d792368435aacd27d97b9674507baac2bca7cf401eea89'
+    Url64 = 'https://github.com/ahmetb/kubectx/releases/download/v0.10.0/kubens_v0.10.0_windows_x86_64.zip'
+    CheckSum64 = '117b81e55231c2c0292d70924c76ba9dda43600f676d479f056130c0b66d4fb58fe7f73e0d8c45ee5a6567a681e7db2dd7b706a05aa8cea144f809a645e536c4'
     CheckSumType64 = 'sha512'
 }
 
