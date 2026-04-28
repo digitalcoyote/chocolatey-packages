4357e784eb7086bb51e4c4e999512855436ff35c
Chocolatey <chocolatey@realdimensions.net>
2026-04-28 00:10:34 +0000

    AU: 1 updated - bazelisk [skip ci]

--- bazelisk/tools/chocolateyinstall.ps1
+++ bazelisk/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'bazelisk.exe'
-    URL64 = 'https://github.com/bazelbuild/bazelisk/releases/download/v1.28.1/bazelisk-windows-amd64.exe'
-    Checksum64 = 'f162168b1672bd7ce30f47bb7cfe7547282e13c987370b9bc455d31b6e165cb94a22810b00200b108c081d1bff38bde4363748ad6daad71e58518f4ecd00e779'
+    URL64 = 'https://github.com/bazelbuild/bazelisk/releases/download/v1.29.0/bazelisk-windows-amd64.exe'
+    Checksum64 = 'eb01121687436a1d04ea63007b01fbca4c5d77cafe71e96e012a7b490f434bfa1362d5a67efca3ff9ed63a9fd7738861308b42d87259b2f1fe37cebf86b85137'
     ChecksumType64 = 'sha512'
     MaxIdleTime = 10000
 }
