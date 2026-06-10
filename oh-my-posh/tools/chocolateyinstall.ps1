f11aa67df6bb9bdf07cd2a48be7211aa780ad0b8
Chocolatey <chocolatey@realdimensions.net>
2026-06-10 00:10:12 +0000

    AU: 1 updated - oh-my-posh [skip ci]

--- oh-my-posh/tools/chocolateyinstall.ps1
+++ oh-my-posh/tools/chocolateyinstall.ps1
@@ -33,8 +33,8 @@ if ($key.Count -eq 1) {
 $InstallArgs = @{ 
     PackageName    = $env:ChocolateyPackageName
     FileType       = 'msi'
-    Url64bit       = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v29.14.0/install-x64.msi'
-    Checksum64     = '5c85fd527a1f3e9e68ee043dceb8ca0ae17cd673ff3676c957b00bfea1cd1eef41584df982090953455bef687e02b8538d06ba2d84ba2a1ae895ce6754d82587'
+    Url64bit       = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v29.15.0/install-x64.msi'
+    Checksum64     = '4897ce55c90c846e4c20e8e43cbe2b69c66a193240661cde7e2991a331d2f086bbfc89a012e7f7b5d0022cd111297aa28bead202cc45c2f8cb164210f44679b3'
     SilentArgs     = $silentArgs 
     ChecksumType64 = 'sha512'
 }
