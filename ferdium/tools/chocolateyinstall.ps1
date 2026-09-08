4f5ef5838ebdb8adf6ba3bfb2ff4670dc74ffd86
Chocolatey <chocolatey@realdimensions.net>
2026-09-08 00:08:30 +0000

    AU: 2 updated - ferdium oh-my-posh [skip ci]

--- ferdium/tools/chocolateyinstall.ps1
+++ ferdium/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $args = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'exe'
   SilentArgs = '/ALLUSERS /S'
-  Url = 'https://github.com/ferdium/ferdium-app/releases/download/v7.2.2/Ferdium-win-AutoSetup-7.2.2-x64.exe'
-  Checksum = '53ce68381cd4c90020fa7cb0cbeeb282be0035c7e1525d7fe1af53ce6e0ab58216381a455c9034384e1e708bf9e992e31b073e99b4a8b1af7f8408ec6e8fb460'
+  Url = 'https://github.com/ferdium/ferdium-app/releases/download/v7.2.3/Ferdium-win-AutoSetup-7.2.3-x64.exe'
+  Checksum = '15ecd8fd3bda1ec3aba605a3940efad871171bde3177d856702c07a6077aa62a48da68f3d445964ff7fc63993bc5a0aa5164fc27a64ec7d4a64cd35a51056e1e'
   ChecksumType = 'sha512'
 }
 
--- oh-my-posh/tools/chocolateyinstall.ps1
+++ oh-my-posh/tools/chocolateyinstall.ps1
@@ -26,8 +26,8 @@ $msixFile  = Join-Path $toolsDir 'install-x64.msix'
 
 $DownloadArgs = @{ 
     PackageName    = $env:ChocolateyPackageName
-    Url64bit       = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v31.1.3/install-x64.msix'
-    Checksum64     = '01b80777f32185df5171adcfb203e45a8a39ce907d8fb641b3099842e3dece01'
+    Url64bit       = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v31.2.0/install-x64.msix'
+    Checksum64     = '8742fa717792cab59904f032a2a25ecc64b2ae8513b0ce520d00b7e163df5e6b'
     ChecksumType64 = 'sha256'
     FileFullPath   = $msixFile
 }
