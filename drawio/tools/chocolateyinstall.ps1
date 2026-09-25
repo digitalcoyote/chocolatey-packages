d384bdb1224fa2ad41542c2fdfe76561eba3fe1b
Chocolatey <chocolatey@realdimensions.net>
2026-09-25 00:08:57 +0000

    AU: 2 updated - drawio sidequest [skip ci]

--- drawio/tools/chocolateyinstall.ps1
+++ drawio/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$drawioversion = '31.4.5'
+$drawioversion = '31.5.2'
 $url        = "https://github.com/jgraph/drawio-desktop/releases/download/v$drawioversion/draw.io-$drawioversion-windows-installer.exe"
 
 $packageArgs = @{
@@ -9,7 +9,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'drawio*'
-  checksum      = 'e0ecc8bb21123e5ebbf8c0c654a0a75f3547bb64b724794e7ff7828a74ba09acb708be55f1503d56a7a867f34a57dee745aaf213127eb8a64806bacb8feee114'
+  checksum      = 'ceecbddfb83e2cba72db37337672840d3016c796be1a0a2cbd262d6b2f1853b16dfef1734f30805162a2b69d1b32c0750082eda9a54e1aef5dca98b2b19771fe'
   checksumType  = 'sha512'
   silentArgs   = '/S'
 }
--- sidequest/tools/chocolateyinstall.ps1
+++ sidequest/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileType = 'exe'
     SilentArgs = '/S'
-    URL64 = 'https://github.com/SideQuestVR/SideQuest/releases/download/v1.2.1/SideQuest-Setup-1.2.1-x64-win.exe'
-    Checksum64 = '8bc3dd91fe12fcc3985bb6cf156d7489ba642eb567be5df48e0e0e82267766f8fe2555a2f968a8a4cf77184e65d5a0eafca9b3e3ed109df4f2fbf7925d148898'
+    URL64 = 'https://github.com/SideQuestVR/SideQuest/releases/download/v1.2.3/SideQuest-Setup-1.2.3-x64-win.exe'
+    Checksum64 = '90cb217260d5fc596a8af4c3e743c2349801ab323d56876772a267375d27cdf064b44e288f7a86f0c768c276976392c35f59163c2c610b07a513357e373b7df2'
     ChecksumType64 = 'sha512'
 }
 
